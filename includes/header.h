bce26bf73c3951b8ea82c5a357bc566ec3eb3f0c
Ferny Yuko <fyuko@at-f4.21-school.ru>
2019-11-12 19:59:59 +0300

    str

--- includes/header.h
+++ includes/header.h
@@ -5,22 +5,44 @@
 # define	BUFF_SIZE	9
 # include "mlx.h"
 # include "math.h"
+# include <fcntl.h>
 # include "../libft/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 
+typedef struct		s_file
+{
+	int				fd;
+	char			*str;
+	int				len;
+	int				res;
+}					t_file;
+
 typedef struct		s_mlx
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	void	*img_ptr;
-	int		x;
-	int		y;
-	int		z;
-	int		size_x;
-	int		size_y;
-	int		*pix_m;
+	void			*mlx_ptr;
+	void			*win_ptr;
+	void			*img_ptr;
+	int				x;
+	int				y;
+	int				z;
+	int				size_x;
+	int				size_y;
+	int				*pix_m;
+	int				bits_per_pixel;
+	int				size_line;
+	int				endian;
 }					t_mlx;
 
+typedef	struct		s_strm
+{
+	char			*s;
+	struct s_strm	*next;
+	
+}					t_strm;
+
+
+void				ft_null(t_mlx *ptr);
 int					get_next_line(const int fd, char **line);
+int					ft_write_image(t_mlx *ptr, char **av);
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -22,32 +22,100 @@ int	some_mouse(int key)
 	return (0);
 }
 
-int	       main(int ac, char **av)
+int		ft_write_image(t_mlx *ptr, char **av)
 {
-	t_mlx	ptr;
-	int i = 0;
-	// int j;
+	int i;
 
-	if (ac != 2)
-		return (0);
-	av++;
-	ptr.mlx_ptr = mlx_init();
-	ptr.win_ptr =  mlx_new_window(ptr.mlx_ptr, WIDTH, HEIGHT, "42");
+	ptr->mlx_ptr = mlx_init();
+	ptr->win_ptr =  mlx_new_window(ptr->mlx_ptr, WIDTH, HEIGHT, "42");
 
-	ptr.img_ptr = mlx_new_image (ptr.mlx_ptr,  WIDTH, HEIGHT);
+	ptr->img_ptr = mlx_new_image (ptr->mlx_ptr,  WIDTH, HEIGHT);
 	
-	ptr.pix_m = (int *)mlx_get_data_addr(ptr.img_ptr, &ptr.x, &ptr.y, &ptr.z);
+	ptr->pix_m = (int *)mlx_get_data_addr(ptr->img_ptr, &(ptr->bits_per_pixel), &(ptr->size_line), &(ptr->endian));
+	i = 0;
+	while(i++ < 800)
+		ptr->pix_m[800 * 300 + i] = 0xFF0000;
+
+	mlx_put_image_to_window(ptr->mlx_ptr, ptr->win_ptr, ptr->img_ptr, 0, 0);
+	
+	mlx_key_hook(ptr->win_ptr, some_key, (void*)0);
+	mlx_mouse_hook(ptr->win_ptr, some_mouse, (void*)0);
+	
+	mlx_loop(ptr->mlx_ptr);
+	mlx_destroy_image(ptr->mlx_ptr, ptr->img_ptr);
+	return (0);
+}
+
+void	ft_null(t_mlx *ptr)
+{
+	ptr->x = 0;
+	ptr->y = 0;
+	ptr->x = 0;
+	ptr->size_x = 0;
+	ptr->size_y = 0;
+	ptr->bits_per_pixel = 0;
+	ptr->size_line = 0;
+	ptr->endian = 0;
+	ptr->mlx_ptr = NULL;
+	ptr->win_ptr = NULL;
+	ptr->img_ptr = NULL;
+	ptr->pix_m = NULL;
+}
+
+int		check_ch(char *av, char c)
+{
+	int	i;
+	int	p;
 
 	i = 0;
-	while(i++ < 500)
-		ptr.pix_m[i] = 0xFF0000;
+	p = 0;
+	while (av[i])
+	{
+		if (av[i] == ' ')
+			++p;
+		++i;
+	}
+	return (i);
+}
 
-	mlx_put_image_to_window(ptr.mlx_ptr, ptr.win_ptr, ptr.img_ptr, 0, 0);
-	
-	mlx_key_hook(ptr.win_ptr, some_key, (void*)0);
-	mlx_mouse_hook(ptr.win_ptr, some_mouse, (void*)0);
-	
-	mlx_loop(ptr.mlx_ptr);
-	mlx_destroy_image(ptr.mlx_ptr, ptr.img_ptr);
+t_strm	*creat_el(char *av)
+{
+	t_strm *tmp;
+
+	if (!(tmp = (t_strm*)malloc(sizeof(t_strm))))
+		exit(0);
+	tmp->s = av;
+	tmp->next = NULL;
+	return (tmp);
+}
+
+int		main(int argc, char **argv)
+{
+	t_mlx	ptr;
+	t_file	file;
+	t_strm	*head_s;
+	t_strm	*tmp;
+
+	ft_null(&ptr);
+	// ft_write_image(&ptr, argv);
+	if (argc != 2)
+		return (0);
+	file.fd = open(argv[1], O_RDONLY);
+	file.res = get_next_line(file.fd, &(file.str));
+	head_s = creat_el(file.str);
+	ptr.size_x = check_ch(head_s->s, ' ');
+	tmp = head_s;
+	while (file.res == 1)
+	{
+		++ptr.size_y;
+		file.res = get_next_line(file.fd, &(file.str));
+		tmp->next = creat_el(file.str);
+		tmp = tmp->next;
+	}
+	while(head_s)
+	{
+		printf("%s\n", head_s->s);
+		head_s = head_s->next;
+	}
 	return (0);
 }
